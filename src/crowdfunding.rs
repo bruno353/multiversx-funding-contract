0d12bc648992c7a6abfd1e6deefae148594e9828
Bruno Santos <brunolsantos152@gmail.com>
2023-12-14 20:50:19 -0300

    Add files via upload

---
+++ src/crowdfunding.rs
@@ -0,0 +1,71 @@
+#![no_std]
+
+multiversx_sc::imports!();
+multiversx_sc::derive_imports!();
+
+#[derive(TopEncode, TopDecode, TypeAbi, PartialEq, Eq, Clone, Copy, Debug)]
+pub enum Status {
+    FundingPeriod,
+    Successful,
+    Failed,
+}
+
+#[multiversx_sc::contract]
+pub trait Crowdfunding {
+
+    #[view(getTarget)]
+    #[storage_mapper("target")]
+    fn target(&self) -> SingleValueMapper<BigUint>;
+  
+    #[view(getDeadline)]
+    #[storage_mapper("deadline")]
+    fn deadline(&self) -> SingleValueMapper<u64>;
+  
+    #[view(getDeposit)]
+    #[storage_mapper("deposit")]
+    fn deposit(&self, donor: &ManagedAddress) -> SingleValueMapper<BigUint>;
+
+    #[endpoint]
+    #[payable("EGLD")]
+    fn fund(&self) {
+        let payment = self.call_value().egld_value();
+
+        let current_time = self.blockchain().get_block_timestamp();
+        require!(current_time < self.deadline().get(), "cannot fund after deadline");
+
+        let caller = self.blockchain().get_caller();
+        self.deposit(&caller).update(|deposit| *deposit += &*payment);
+    }
+
+    #[endpoint]
+    fn claim(&self) {
+        match self.status() {
+            Status::FundingPeriod => sc_panic!("cannot claim before deadline"),
+            Status::Successful => {
+                let caller = self.blockchain().get_caller();
+                require!(
+                    caller == self.blockchain().get_owner_address(),
+                    "only owner can claim successful funding"
+                );
+
+                let sc_balance = self.get_current_funds();
+                self.send().direct_egld(&caller, &sc_balance);
+            },
+            Status::Failed => {
+                let caller = self.blockchain().get_caller();
+                let deposit = self.deposit(&caller).get();
+
+                if deposit > 0u32 {
+                    self.deposit(&caller).clear();
+                    self.send().direct_egld(&caller, &deposit);
+                }
+            },
+        }
+    }
+
+    #[init]
+    fn init(&self, target: BigUint, deadline: u64) {
+        self.target().set(&target);
+        self.deadline().set(&deadline);
+    }
+}
